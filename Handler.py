1a7a6c8546043b478008cd995f120898cce82243
Zac Bagley <zbagley610@gmail.com>
2021-01-30 23:19:57 -0500

    formatting

---
+++ Handler.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+inputdir = '/home/user/input'
+#path of the folder containing jpgs to move
+outputdir = '/home/user/output'
+#path of folder to put jpgs
+
+for filename in os.listdir(inputdir):
+#iterates through every file in indputdir, stores file name in filename variable
+    if os.path.splitext(filename)[1] == ".jpg":
+    #checks if file extension is .jpg. if not, skips file
+        with open('test.txt', 'a') as file:
+        #opens text file named test.txt in append mode, text file must exist
+            file.write('<img src="documents/photos/{}" alt="Something or another">'.format(filename))
+            #writes img tag to new line of text.txt, {} is placeholder which is filled by filename variable of the .format(filename) syntax
+        shutil.copy2(os.path.join(inputdir, filename), outputdir)
+        #copies file from inputdir path to outputdir path
