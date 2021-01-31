e26d1c93ceae88ce474887d95dbcb74020bc888b
Zac Bagley <59242014+Binxly@users.noreply.github.com>
2021-01-31 00:23:42 -0500

    Update Handler.py

--- Handler.py
+++ Handler.py
@@ -1,9 +1,9 @@
 import os
 import shutil
 
-inputdir = 'D:\crext\Documents\GitHub\ImageLoader\ImageLoader\input'
+inputdir = 'C:\user\Photos\input'
 #path of the folder containing jpgs to move
-outputdir = 'D:\crext\Documents\GitHub\ImageLoader\ImageLoader\output'
+outputdir = 'C:\user\Photos\output'
 #path of folder to put jpgs
 
 file = open("test.txt", "a")
@@ -13,7 +13,7 @@ for filename in os.listdir(inputdir):
 #iterates through every file in indputdir, stores file name in filename variable
     if os.path.splitext(filename)[1] == ".jpg":
     #checks if file extension is .jpg. if not, skips file
-        file.write('<img src="documents/photos/{}" alt="Something or another">'.format(filename))
+        file.write('<img src="documents/photos/{}" alt="Example">'.format(filename))
         #writes img tag to new line of text.txt, {} is placeholder which is filled by filename variable of the .format(filename) syntax
         shutil.copy2(os.path.join(inputdir, filename), outputdir)
         #copies file from inputdir path to outputdir path
