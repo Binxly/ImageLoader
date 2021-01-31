82461abc23f1e6cc332a2d725af1103d43b26cb6
Zac Bagley <zbagley610@gmail.com>
2021-01-30 22:53:13 -0500

    script to send files to output dir and write img tag to text doc

--- Modify.py
+++ Modify.py
@@ -1,2 +1,13 @@
-with open('something.html', 'w') as something_file: 
-   something_file.write('<!DOCTYPE html><html><body>')
+import os
+import shutil
+
+inputdir = '/home/user/input'
+outputdir = '/home/user/output'
+
+
+
+for filename in os.listdir(inputdir):
+    if os.path.splitext(filename)[1] == ".jpg":
+        with open('test.txt', 'a') as file:
+            file.write('<img src="documents/photos/{}" alt="Something or another">'.format(filename))
+        shutil.copy2(os.path.join(inputdir, filename), outputdir)
