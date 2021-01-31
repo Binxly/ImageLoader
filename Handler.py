8f65b91fa2b9a103600592b2cdffbc656e26ddd6
Zac Bagley <zbagley610@gmail.com>
2021-01-30 23:26:29 -0500

    optimized script

--- Handler.py
+++ Handler.py
@@ -6,13 +6,16 @@ inputdir = 'D:\crext\Documents\GitHub\ImageLoader\ImageLoader\input'
 outputdir = 'D:\crext\Documents\GitHub\ImageLoader\ImageLoader\output'
 #path of folder to put jpgs
 
+file = open("test.txt", "a")
+#opens text file named test.txt in append mode, text file must exist
+
 for filename in os.listdir(inputdir):
 #iterates through every file in indputdir, stores file name in filename variable
     if os.path.splitext(filename)[1] == ".jpg":
     #checks if file extension is .jpg. if not, skips file
-        with open('test.txt', 'a') as file:
-        #opens text file named test.txt in append mode, text file must exist
-            file.write('<img src="documents/photos/{}" alt="Something or another">'.format(filename))
-            #writes img tag to new line of text.txt, {} is placeholder which is filled by filename variable of the .format(filename) syntax
+        file.write('<img src="documents/photos/{}" alt="Something or another">'.format(filename))
+        #writes img tag to new line of text.txt, {} is placeholder which is filled by filename variable of the .format(filename) syntax
         shutil.copy2(os.path.join(inputdir, filename), outputdir)
         #copies file from inputdir path to outputdir path
+
+file.close()
