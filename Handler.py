cc0840a3212ae2907564b240462d5a780010f14c
Zac Bagley <zbagley610@gmail.com>
2021-02-02 14:07:59 -0500

    Update Handler.py

--- Handler.py
+++ Handler.py
@@ -18,7 +18,7 @@ for filename in os.listdir(inputdir):
 #iterates through every file in indputdir, stores file name in filename variable
     if os.path.splitext(filename)[1] == ".jpg":
     #checks if file extension is .jpg. if not, skips file
-        file.write('<img src="documents/photos/{}" alt="Example">'.format(filename))
+        file.write('<img src="documents/photos/{}" alt="Example">'.format(filename)) #CHANGE SRC TO YOUR HTML PAGE'S IMAGE DIRECTORY
         #writes img tag to new line of text.txt, {} is placeholder which is filled by filename variable of the .format(filename) syntax
         shutil.copy2(os.path.join(inputdir, filename), outputdir)
         #copies file from inputdir path to outputdir path
